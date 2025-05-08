e6ee1d01ce9cc283531def4801b3059d2106213c
yuukikounishi <happywestyuki@gmail.com>
2025-05-08 22:41:05 +0900

    Create Xcode project file

---
+++ VisionPulse/VisionPulse/VisionPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionPulseApp.swift
+//  VisionPulse
+//
+//  Created by yuuki kounishi on 2025/05/08.
+//
+
+import SwiftUI
+
+@main
+struct VisionPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionPulse/VisionPulseTests/VisionPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionPulseTests.swift
+//  VisionPulseTests
+//
+//  Created by yuuki kounishi on 2025/05/08.
+//
+
+import Testing
+@testable import VisionPulse
+
+struct VisionPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
